ae7f006611eb59fc7270ecbeef68dc5ce7df9bd3
mkrasnick <MKrasnick@enservio.com>
2015-04-04 18:13:26 -0400

    initial commit

---
+++ app/scripts/app.config.js
@@ -0,0 +1,19 @@
+angular
+    .module('psJwtApp').config(function($urlRouterProvider, $stateProvider){
+
+    $urlRouterProvider.otherwise('/');
+        $stateProvider
+          .state('main', {
+            url: '/',
+            templateUrl: 'views/main.html'
+          })
+
+          .state('register', {
+            url: '/register',
+            templateUrl: 'views/register.html',
+            controller: 'RegisterCtrl'
+        });
+    })
+/**
+ * Created by mkrasnick on 03/25/2015.
+ */
